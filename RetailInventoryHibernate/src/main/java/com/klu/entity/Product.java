5256585b9f8af278fad83c221bd507484897f1d2
Sravya-3011 <2400032052@kluniversity.in>
2026-01-07 09:25:54 +0530

    commit
    commit

---
+++ RetailInventoryHibernate/src/main/java/com/klu/entity/Product.java
@@ -0,0 +1,43 @@
+package com.klu.entity;
+
+import javax.persistence.*;
+
+@Entity                 // tells Hibernate this is a table
+@Table(name = "products")
+public class Product {
+
+    @Id                // Primary Key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+
+    // Default constructor (mandatory)
+    public Product() {}
+
+    // Parameterized constructor
+    public Product(String name, String description, double price, int quantity) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // Getters & Setters
+    public int getId() { return id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+}
