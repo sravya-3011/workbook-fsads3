1552b0c180af9e7b6573549beea641457e899d1e
Sravya-3011 <2400032052@kluniversity.in>
2026-03-06 23:36:47 +0530

    commit
    commit

---
+++ Experiment-9/src/main/java/com/klu/Experiment9Application.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Experiment9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Experiment9Application.class, args);
+	}
+
+}
